bd172064c4474f0b4a292e8dd6d9099ecad49c78
Ben Gonzales <benedicto.gonzales@hp.com>
2021-10-19 02:46:18 -0500

    init

---
+++ src/test/java/com/smart/apartments/leasing/LeasingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.apartments.leasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
